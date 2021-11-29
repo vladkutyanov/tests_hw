32850b9f1000fc85a20fed9c18afeb0958c2f306
vladkutyanov <71340957+vladkutyanov@users.noreply.github.com>
2021-11-29 20:56:38 +0300

    Update test_issue_05.py

--- issue-05/test_issue_05.py
+++ issue-05/test_issue_05.py
@@ -5,9 +5,8 @@ import pytest
 import get_year
 
 
-
 @patch('urllib.request.urlopen')
-def test_url_YMD(mock_urlopen):
+def test_url_ymd(mock_urlopen):
     cm = MagicMock()
     cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '2020-11-29T1'})
     cm.__enter__.return_value = cm
@@ -15,14 +14,16 @@ def test_url_YMD(mock_urlopen):
     with urllib.request.urlopen('http://localhost'):
         assert get_year.what_is_year_now() == 2020
 
+
 @patch('urllib.request.urlopen')
-def test_url_DMY(mock_urlopen):
+def test_url_dmy(mock_urlopen):
     cm = MagicMock()
     cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '29.11.2020'})
     cm.__enter__.return_value = cm
     mock_urlopen.return_value = cm
     with urllib.request.urlopen('http://localhost'):
         assert get_year.what_is_year_now() == 2020
+
 
 @patch('urllib.request.urlopen')
 def test_url_invalid(mock_urlopen):
@@ -32,8 +33,3 @@ def test_url_invalid(mock_urlopen):
     mock_urlopen.return_value = cm
     with urllib.request.urlopen('http://localhost'):
         assert get_year.what_is_year_now() == 2020
-
-
-
-
-
