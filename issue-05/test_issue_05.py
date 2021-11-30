523a43935457ad596b60f888b6df39b6c62fe224
vladkutyanov <71340957+vladkutyanov@users.noreply.github.com>
2021-11-30 10:41:53 +0300

    Add files via upload

---
+++ issue-05/test_issue_05.py
@@ -0,0 +1,33 @@
+import urllib.request
+import json
+from unittest.mock import patch, MagicMock
+import pytest
+import get_year
+
+
+@patch('urllib.request.urlopen')
+def test_url_ymd(mock_urlopen):
+    cm = MagicMock()
+    cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '2020-11-29T1'})
+    cm.__enter__.return_value = cm
+    mock_urlopen.return_value = cm
+    assert get_year.what_is_year_now() == 2020
+
+
+@patch('urllib.request.urlopen')
+def test_url_dmy(mock_urlopen):
+    cm = MagicMock()
+    cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '29.11.2020'})
+    cm.__enter__.return_value = cm
+    mock_urlopen.return_value = cm
+    assert get_year.what_is_year_now() == 2020
+
+
+@patch('urllib.request.urlopen')
+def test_url_invalid(mock_urlopen):
+    cm = MagicMock()
+    cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '29-1134-2020'})
+    cm.__enter__.return_value = cm
+    mock_urlopen.return_value = cm
+    with pytest.raises(Exception):
+        assert get_year.what_is_year_now() == 2020
