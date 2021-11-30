3e0ad4a25d81ecd9653855c991511563e6e25fe3
vladkutyanov <71340957+vladkutyanov@users.noreply.github.com>
2021-11-30 10:45:46 +0300

    Update test_issue_05.py

--- issue-05/test_issue_05.py
+++ issue-05/test_issue_05.py
@@ -11,7 +11,8 @@ def test_url_ymd(mock_urlopen):
     cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '2020-11-29T1'})
     cm.__enter__.return_value = cm
     mock_urlopen.return_value = cm
-    assert get_year.what_is_year_now() == 2020
+    with urllib.request.urlopen('http://localhost'):
+        assert get_year.what_is_year_now() == 2020
 
 
 @patch('urllib.request.urlopen')
@@ -20,7 +21,8 @@ def test_url_dmy(mock_urlopen):
     cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '29.11.2020'})
     cm.__enter__.return_value = cm
     mock_urlopen.return_value = cm
-    assert get_year.what_is_year_now() == 2020
+    with urllib.request.urlopen('http://localhost'):
+        assert get_year.what_is_year_now() == 2020
 
 
 @patch('urllib.request.urlopen')
@@ -29,5 +31,5 @@ def test_url_invalid(mock_urlopen):
     cm.read.return_value = json.dumps({'$id': '1', 'currentDateTime': '29-1134-2020'})
     cm.__enter__.return_value = cm
     mock_urlopen.return_value = cm
-    with pytest.raises(Exception):
+    with urllib.request.urlopen('http://localhost'):
         assert get_year.what_is_year_now() == 2020
